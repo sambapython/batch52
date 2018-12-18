dd982d1dd56c23efd6bca11040aa45e02485591c
sambapython <sambapython@gmail.com>
2018-12-18 09:12:00 +0530

    initial commit

---
+++ sample.py
@@ -0,0 +1,13 @@
+import time
+s1="python"
+print ("s1",s1)
+time.sleep(10)
+s2=s1
+print ("s2",s2)
+time.sleep(1)
+s3="apple"
+print ("s3",s3)
+time.sleep(1)
+s4="python"
+print ("s4",s4)
+time.sleep(1)
