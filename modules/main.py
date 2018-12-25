359495840ab5abaa208bba2a7cc54c302385adde
sambapython <sambapython@gmail.com>
2018-12-25 08:31:44 +0530

    dict and modules

--- modules/main.py
+++ modules/main.py
@@ -10,6 +10,21 @@ import pur
 import sales
 sales.create_customer()
 '''
+'''
 from sales import create_customer
 create_customer()
+'''
+'''
+import sales
+sales.create_customer()
+'''
+"""
+import sys
+#sys.path.append(r"C:UsersGUDURUDesktop")
+#sys.path.insert(1,r"C:UsersGUDURUDesktop")
+print(sys.path)
+import sales
+"""
+import sales
+sales.create_customer()
 
--- modules/sales.py
+++ modules/sales.py
@@ -1,12 +1,13 @@
-
-print("this is sales file")
+sales="value"
 def create_customer():
+	global __a__
     print("createing customer completed")
 
 def main():
     print("this is sale file")
     print ("name:",__name__)
     create_customer()
-
-if __name__=="__main__":
-    main()
+if __name__ == "__main__":
+	main()
+	print("this is sales file")
+	print ("sales from")
